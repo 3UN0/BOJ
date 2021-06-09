2c7d1a197438124d8dd0bcd50db2348c8f16d1b8
nyoung <seosilverzero@gmail.com>
2021-06-09 21:12:39 +0900

    Update Boj_2751.java

--- Boj_2751.java
+++ Boj_2751.java
@@ -10,6 +10,7 @@ public class Boj_2751 {
 
 		int N = Integer.parseInt(br.readLine());
 		
+		//Collections.sort() 이용하기 위해선 list 배열 필요함
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
 		for(int i=0;i<N;i++) {
@@ -18,6 +19,7 @@ public class Boj_2751 {
 
 		Collections.sort(list);
 		
+		// sb 이용하여 시간단축 
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i=0;i<N;i++) {
