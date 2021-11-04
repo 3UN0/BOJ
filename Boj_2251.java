01c539046942e9244b6829d86e6ff9b0a442f5fc
nyoung <seosilverzero@gmail.com>
2021-11-04 23:57:39 +0900

    [BOJ]DFS/BFS-물통 (BFS)

---
+++ Boj_2251.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Test {
+	public static int[][] arr;
+	public static int N, count;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		
+		
+		
+	}
+}
