6c39b3ffda2bc644f3ddb1c35f6abfc676492a84
nyoung <seosilverzero@gmail.com>
2021-12-10 01:18:20 +0900

    [BOJ]DFS/BFS-맥주 마시면서 걸어가기 (플로이드 마샬)

--- Boj_9205.java
+++ Boj_9205.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Test {
@@ -12,8 +12,11 @@ public class Test {
 	public static int[] dx = {-1, 0, 1, 0};	// 북0 동1 남2 서3
 	public static int[] dy = {0, 1, 0, -1};
 	
+	// 플로이드 와샬 알고리즘 이용
+	// 모든 정점에 대한 거리 조사 -> 도달하는 과정에서 편의점 있나 없나 검사
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		StringTokenizer st;
 		
@@ -23,69 +26,49 @@ public class Test {
 			
 			// 편의점 개수
 			int n = Integer.parseInt(br.readLine());
+			List<int[]> list = new ArrayList<>();
+			for(int j=0;j<n+2;j++) {
+				st = new StringTokenizer(br.readLine());
+				// 좌표
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				list.add(new int[] {x, y});
+			}
+			// 편의점 유무 확인
+			boolean[][] flag = new boolean[n+2][n+2];
+			for(int j=0;j<n+2;j++) {
+				for(int k=0;k<n+2;k++) {
+					int[] now = list.get(j);
+					int[] next = list.get(k);
+					// 좌표 사이 거리
+					int dis = Math.abs(now[0] - next[0]) + Math.abs(now[1] - next[1]);
+					
+					if(dis <= 1000) {
+						flag[j][k] = true;
+					}
+				}
+			}
 			
-			st = new StringTokenizer(br.readLine());
-			// 상근이 집의 좌표
-			int hx = Integer.parseInt(st.nextToken());
-			int hy = Integer.parseInt(st.nextToken());
-
-			st = new StringTokenizer(br.readLine());
-			// 편의점의 좌표
-			int px = Integer.parseInt(st.nextToken());
-			int py = Integer.parseInt(st.nextToken());
-
-			st = new StringTokenizer(br.readLine());
-			// 락페의 좌표
-			int rx = Integer.parseInt(st.nextToken());
-			int ry = Integer.parseInt(st.nextToken());
+			for(int j=0;j<n+2;j++) {
+				for(int k=0;k<n+2;k++) {
+					for(int m=0;m<n+2;m++) {
+						if(flag[k][j] && flag[j][m]) {
+							flag[k][m] = true;
+						}
+					}
+				}
+			}
 			
-			
-			
+			if(flag[0][n+1]) {
+				sb.append("happy\n");
+			} else {
+				sb.append("sad\n");
+			}
 			
 		}
+		System.out.println(sb);
 				
-		// 1층부터 F층까지
-		int[] floor = new int[F+1];
-		
-		System.out.println(bfs(S, G, floor));
-		
 	}
 	
-	// bfs로 시작점 큐에 넣어서 시작 -> up, down 이동경로 큐에 저장 -> poll했을 때 위치가 도착점이면 끝
-	// 위치가 0 보다 작거나 F보다 크면 return
-	public static String bfs(int start, int end, int[] arr) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(start);
-		// 처음 시작 버튼 누르고 시작한다 가정
-		arr[start] = 1;
-		
-		while(!q.isEmpty()) {
-			int current = q.poll();
-			// 현재 위치와 도착지 비교
-			if(current == end) {
-				return String.valueOf(arr[current] - 1);
-			}
-			
-			// 다음 up 위치가 최대 높이보다 작고, 방문 x 지점
-			if(current + U <= F) {
-				if(arr[current+U] == 0) {
-					arr[current+U] = arr[current] + 1;
-					q.add(current+U);
-				}
-			}
-			
-			// 다음 down 위치가 1층 이상이고, 방문 x 위치
-			if(current - D > 0) {
-				if(arr[current-D] == 0) {
-					arr[current-D] = arr[current] + 1;
-					q.add(current - D);
-				}
-			}
-		}
-		return "use the stairs";
-	
-	
-	}
-	
-	
+	// https://loosie.tistory.com/334 참고
 }
